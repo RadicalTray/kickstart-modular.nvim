9082444527519b1e7230fd7932edcbd62e693bed
Patipon Sanor <6633134721@student.chula.ac.th>
2024-04-25 01:06:04 +0700

    Change Telescope ui-select theme

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -103,7 +103,7 @@ return {
         },
         extensions = {
           ['ui-select'] = {
-            require('telescope.themes').get_dropdown(),
+            require('telescope.themes').get_cursor(),
           },
         },
       }
