cd2bc4f0eb1f5c5609a2ccf33fbdfce04a5d08e9
Patipon Sanor <6633134721@student.chula.ac.th>
2024-04-30 03:27:58 +0700

    fix the added transparent stuff
    so the load order of linefly and transparent doesn't matter???

--- lua/custom/plugins/transparent.lua
+++ lua/custom/plugins/transparent.lua
@@ -5,13 +5,18 @@ return {
     extra_groups = { 'WinSeparator' },
     -- exclude_groups = { 'StatusLine', 'StatusLineNC' },
     --
-    -- NOTE: The loading order of linefly and transparent.nvim matters
+    -- NOTE: The loading order of linefly and transparent.nvim matters ?????
+    --       I don't know anymore, adding/removing clear_prefix doesn't instantly take effect after reboot
   },
   config = function(_, opts)
     require('transparent').setup(opts)
 
+    -- make sure to clear_prefix of StatusLine, so it is transparent, but no highlights D:
+    require('transparent').clear_prefix 'StatusLine'
+    -- this seems to affects both StatusLine and StatusLineNC
+
     -- can also put this in options.lua maybe??
     vim.opt.fillchars = { vert = '⠀', eob = '⠀' }
-    -- * NOT a whitespace, it's U+2800 *
+    -- * NOT a space, it's U+2800 *
   end,
 }
