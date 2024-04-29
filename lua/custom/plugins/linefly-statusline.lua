c6d685ec0cdbf8200121bb2ceec76ceb2e934bc9
Patipon Sanor <6633134721@student.chula.ac.th>
2024-04-30 03:09:43 +0700

    make more things transparent

--- lua/custom/plugins/linefly-statusline.lua
+++ lua/custom/plugins/linefly-statusline.lua
@@ -1,4 +1,7 @@
 return {
   'bluz71/nvim-linefly',
   lazy = false,
+  priority = 999,
+  -- To make sure this loads before transparent,
+  -- and probably preferably after moonfly (main colorscheme) I guess
 }
--- lua/custom/plugins/transparent.lua
+++ lua/custom/plugins/transparent.lua
@@ -3,6 +3,15 @@ return {
   lazy = false,
   opts = {
     extra_groups = { 'WinSeparator' },
-    exclude_groups = { 'StatusLine', 'StatusLineNC' },
+    -- exclude_groups = { 'StatusLine', 'StatusLineNC' },
+    --
+    -- NOTE: The loading order of linefly and transparent.nvim matters
   },
+  config = function(_, opts)
+    require('transparent').setup(opts)
+
+    -- can also put this in options.lua maybe??
+    vim.opt.fillchars = { vert = '⠀', eob = '⠀' }
+    -- * NOT a whitespace, it's U+2800 *
+  end,
 }
