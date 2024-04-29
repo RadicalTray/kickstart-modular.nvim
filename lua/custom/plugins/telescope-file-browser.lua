3cc0739a3cd8108e5e70a1a0c569ff5cb5286f7f
Patipon Sanor <6633134721@student.chula.ac.th>
2024-04-29 20:42:55 +0700

    file browser opens in the same directory as current buffer

--- lua/custom/plugins/telescope-file-browser.lua
+++ lua/custom/plugins/telescope-file-browser.lua
@@ -2,14 +2,13 @@ return {
   'nvim-telescope/telescope-file-browser.nvim',
   dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
   keys = {
-    { '<space>ob', '<cmd>Telescope file_browser<CR>', desc = '[O]pen file [B]rowser', noremap = true },
+    { '<space>ob', '<cmd>Telescope file_browser path=%:p:h select_buffer=true<CR>', desc = '[O]pen file [B]rowser' },
   },
   config = function()
     require('telescope').setup {
       extensions = {
         file_browser = {
-          cwd_to_path = true,
-          follow_symlinks = true, -- why is this not true by default? does this break something?
+          follow_symlinks = true,
           respect_gitignore = false,
           hijack_netrw = true,
         },
