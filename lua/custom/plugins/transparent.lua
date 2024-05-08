c43ee35d0bddfec18f2bf11c1bb7f04bcbb98e8b
Patipon Sanor <6633134721@student.chula.ac.th>
2024-05-09 00:16:15 +0700

    change transparent config and set timeout to false

--- lua/custom/plugins/transparent.lua
+++ lua/custom/plugins/transparent.lua
@@ -3,7 +3,7 @@ return {
   lazy = false,
   opts = {
     extra_groups = { 'WinSeparator' },
-    -- exclude_groups = { 'StatusLine', 'StatusLineNC' },
+    exclude_groups = { 'StatusLine', 'StatusLineNC' },
     --
     -- NOTE: The loading order of linefly and transparent.nvim matters ?????
     --       I don't know anymore, adding/removing clear_prefix doesn't instantly take effect after reboot
@@ -12,7 +12,7 @@ return {
     require('transparent').setup(opts)
 
     -- make sure to clear_prefix of StatusLine, so it is transparent, but no highlights D:
-    require('transparent').clear_prefix 'StatusLine'
+    -- require('transparent').clear_prefix 'StatusLine'
     -- this seems to affects both StatusLine and StatusLineNC
 
     -- can also put this in options.lua maybe??
--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -127,10 +127,7 @@ return {
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-
-      -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
-        -- You can pass additional configuration to Telescope to change the theme, layout, etc.
         builtin.current_buffer_fuzzy_find { previewer = false }
       end, { desc = '[/] Fuzzily search in current buffer' })
 
