2068a4bf3ca23e0edaeee4c3a7fba89e35659efc
Patipon Sanor <6633134721@student.chula.ac.th>
2024-05-12 18:30:15 +0700

    Restructure files

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -1,46 +1,6 @@
 -- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', lazy = false, opts = {} },
-
-  require 'kickstart/plugins/gitsigns',
-
-  -- require 'kickstart/plugins/which-key',
-
-  require 'kickstart/plugins/telescope',
-
-  require 'kickstart/plugins/lspconfig',
-
-  require 'kickstart/plugins/conform',
-
-  require 'kickstart/plugins/cmp',
-
-  -- require 'kickstart/plugins/tokyonight',
-  require 'custom/moonfly',
-
-  require 'kickstart/plugins/todo-comments',
-
-  -- require 'kickstart/plugins/mini',
-
-  require 'kickstart/plugins/treesitter',
-
-  require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-
-  { import = 'custom.plugins' },
+  { import = 'plugins.enabled' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
---
+++ lua/plugins/enabled/Comment.lua
@@ -0,0 +1,2 @@
+return { 'numToStr/Comment.nvim', event = 'VimEnter', opts = {} }
+-- "gc" to comment visual regions/lines
