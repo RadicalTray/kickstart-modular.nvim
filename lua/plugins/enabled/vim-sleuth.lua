688df9d7be0f965008db6d1d440400f210582714
radicaltray <hydrochloricmahn@gmail.com>
2024-05-14 14:43:20 +0700

    Format in vim-sleuth

--- lua/plugins/enabled/vim-sleuth.lua
+++ lua/plugins/enabled/vim-sleuth.lua
@@ -1,2 +1,5 @@
-return { 'tpope/vim-sleuth', event = { 'BufReadPost', 'BufNewFile' } }
--- Detect tabstop and shiftwidth automatically
+return {
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+  event = { 'BufReadPost', 'BufNewFile' }, -- does this break the plugin?
+}
