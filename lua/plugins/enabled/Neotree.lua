fd27535bd421eaff83c6826c057bac12631cfd50
RadicalTray <radicaltray@gmail.com>
2024-05-15 20:58:47 +0700

    Add Neotree

---
+++ lua/plugins/enabled/Neotree.lua
@@ -0,0 +1,25 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'MunifTanjim/nui.nvim',
+    -- "3rd/image.nvim", -- Optional image support in preview window
+  },
+  keys = {
+    { '<leader>oft', '<cmd>Neotree<CR>', desc = '[O]pen [F]ile [T]ree' },
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true, -- makes the 'hidden' files display differently
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_hidden = false, -- only works on Windows for hidden files/directories
+      },
+      group_empty_dirs = true,
+      hijack_netrw_behavior = 'disabled',
+    },
+  },
+}
