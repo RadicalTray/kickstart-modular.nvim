9c17f81b01d485b92aa5f449dc4173af54430cfd
RadicalTray <radicaltray@gmail.com>
2024-05-15 20:29:09 +0700

    Set Neogit to track nightly branch + more keymap

--- lua/plugins/enabled/neogit.lua
+++ lua/plugins/enabled/neogit.lua
@@ -1,10 +1,14 @@
 return {
   'NeogitOrg/neogit',
+  branch = 'nightly',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'sindrets/diffview.nvim',
     'nvim-telescope/telescope.nvim',
   },
   cmd = 'Neogit',
+  keys = {
+    { '<leader>ng', '<cmd>Neogit<CR>', desc = 'Open [N]eo[G]it' },
+  },
   config = true,
 }
