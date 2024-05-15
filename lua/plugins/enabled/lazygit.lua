da9467715f9b0aa483eb90999074f079bb706e74
RadicalTray <radicaltray@gmail.com>
2024-05-15 20:22:48 +0700

    Change Lazygit keymap

--- lua/plugins/enabled/lazygit.lua
+++ lua/plugins/enabled/lazygit.lua
@@ -14,6 +14,14 @@ return {
   -- setting the keybinding for LazyGit with 'keys' is recommended in
   -- order to load the plugin when the command is run for the first time
   keys = {
-    { '<leader>og', '<cmd>LazyGit<cr>', desc = '[O]pen Lazy[G]it' },
+    { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'Open [L]azy[G]it' },
+    -- { '', '<cmd>lua require("telescope").extensions.lazygit.lazygit()<cr>', desc = '' },
   },
+  -- for opening git submodules with telescope and lazygit
+  -- config = function()
+  --   require('telescope').load_extension 'lazygit'
+  -- end,
+  --
+  -- it also probably needs this autocmd for it to work properly with lazyloading
+  -- autocmd BufEnter * :lua require('lazygit.utils').project_root_dir()
 }
