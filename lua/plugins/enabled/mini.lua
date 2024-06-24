fbfd272affdf9df4e2b52c7381be5bffeb3dccdf
RadicalTray <radicaltray@gmail.com>
2024-06-24 11:28:07 +0700

    gitsigns.nvim -> mini.diff

--- lua/plugins/enabled/mini.lua
+++ lua/plugins/enabled/mini.lua
@@ -1,12 +1,20 @@
 return {
   {
     'echasnovski/mini.nvim',
-    event = 'VeryLazy',
+    event = { 'BufReadPost', 'BufNewFile' },
     version = false,
     config = function()
       require('mini.ai').setup {}
       require('mini.surround').setup {}
-      require('mini.git').setup()
+      require('mini.git').setup {}
+      require('mini.diff').setup {
+        view = {
+          style = 'sign',
+          signs = { add = '┃', change = '┃', delete = '_' },
+        },
+      }
+
+      vim.keymap.set('n', '<leader>gh', MiniDiff.toggle_overlay, { desc = 'Toggle [G]it [H]unk overlay' })
     end,
   },
 }
