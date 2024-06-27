7fcca879c409feee1348a923c48f953b592c07b3
RadicalTray <radicaltray@gmail.com>
2024-06-27 16:58:38 +0700

    set mousescroll

--- lua/options.lua
+++ lua/options.lua
@@ -10,6 +10,7 @@ vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
+vim.opt.mousescroll = 'ver:1,hor:1'
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
