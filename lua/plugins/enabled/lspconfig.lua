bbe275decbd56c9aad2b4e2f0865396677a5cde0
RadicalTray <hydrochloricmahn@gmail.com>
2024-08-24 22:23:01 +0700

    separate mason from lspconfig

--- lua/plugins/enabled/lspconfig.lua
+++ lua/plugins/enabled/lspconfig.lua
@@ -2,17 +2,7 @@ return { -- LSP Configuration & Plugins
   'neovim/nvim-lspconfig',
   enabled = true,
   dependencies = {
-    -- Automatically install LSPs and related tools to stdpath for Neovim
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
-
-    -- Useful status updates for LSP.
-    -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
     { 'j-hui/fidget.nvim', opts = {} },
-
-    -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
-    -- used for completion, annotations and signatures of Neovim apis
     { 'folke/neodev.nvim', opts = {} },
   },
   config = function()
@@ -50,71 +40,6 @@ return { -- LSP Configuration & Plugins
         end
       end,
     })
-
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-
-    local servers = {
-      lua_ls = {
-        settings = {
-          Lua = {
-            completion = {
-              callSnippet = 'Replace',
-            },
-            -- diagnostics = { disable = { 'missing-fields' } },
-          },
-        },
-      },
-      omnisharp = {
-        settings = {
-          FormattingOptions = {
-            EnableEditorConfigSupport = true,
-            OrganizeImports = true,
-          },
-          MsBuild = {
-            LoadProjectsOnDemand = false,
-          },
-          RoslynExtensionsOptions = {
-            EnableAnalyzersSupport = false,
-            EnableImportCompletion = true,
-            AnalyzeOpenDocumentsOnly = true,
-          },
-          Sdk = {
-            IncludePrereleases = true,
-          },
-        },
-      },
-      vtsls = {},
-      eslint = {},
-      cssls = {},
-      tailwindcss = {},
-      html = {},
-      jdtls = {},
-      bashls = {},
-      pylsp = {},
-      clangd = {},
-    }
-
-    require('mason').setup()
-
-    local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      'stylua',
-      'beautysh',
-      'prettierd',
-      'clang-format',
-    })
-    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-    require('mason-lspconfig').setup {
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          require('lspconfig')[server_name].setup(server)
-        end,
-      },
-    }
   end,
 }
 -- vim: ts=2 sts=2 sw=2 et
