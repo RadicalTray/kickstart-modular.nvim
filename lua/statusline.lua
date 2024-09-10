098545b9566e2b0c825e448f7995541cf2c01780
RadicalTray <hydrochloricmahn@gmail.com>
2024-09-11 01:02:32 +0700

    stl :D

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -1,25 +1,31 @@
-local modes = {
-  ['n'] = 'NORMAL',
-  ['no'] = 'NORMAL',
-  ['v'] = 'VISUAL',
-  ['V'] = 'VISUAL LINE',
-  [''] = 'VISUAL BLOCK',
-  ['s'] = 'SELECT',
-  ['S'] = 'SELECT LINE',
-  [''] = 'SELECT BLOCK',
-  ['i'] = 'INSERT',
-  ['ic'] = 'INSERT',
-  ['R'] = 'REPLACE',
-  ['Rv'] = 'VISUAL REPLACE',
-  ['c'] = 'COMMAND',
-  ['cv'] = 'VIM EX',
-  ['ce'] = 'EX',
-  ['r'] = 'PROMPT',
-  ['rm'] = 'MOAR',
-  ['r?'] = 'CONFIRM',
-  ['!'] = 'SHELL',
-  ['t'] = 'TERMINAL',
-}
+-- local modes = {
+--   ['n'] = 'NORMAL',
+--   ['no'] = 'O-PENDING',
+--   ['nov'] = 'O-PENDING',
+--   ['noV'] = 'O-PENDING',
+--   ['noCTRL-V'] = 'CTRL-V',
+--   ['niI'] = 'CTRL-O',
+--   ['niR'] = 'CTRL-O',
+--   ['niV'] = 'CTRL-O',
+--   ['v'] = 'VISUAL',
+--   ['V'] = 'VISUAL LINE',
+--   [''] = 'VISUAL BLOCK',
+--   ['s'] = 'SELECT',
+--   ['S'] = 'SELECT LINE',
+--   [''] = 'SELECT BLOCK',
+--   ['i'] = 'INSERT',
+--   ['ic'] = 'INSERT',
+--   ['R'] = 'REPLACE',
+--   ['Rv'] = 'VISUAL REPLACE',
+--   ['c'] = 'COMMAND',
+--   ['cv'] = 'VIM EX',
+--   ['ce'] = 'EX',
+--   ['r'] = 'PROMPT',
+--   ['rm'] = 'MOAR',
+--   ['r?'] = 'CONFIRM',
+--   ['!'] = 'SHELL',
+--   ['t'] = 'TERMINAL',
+-- }
 
 function Get_stl_mode()
   local m = vim.api.nvim_get_mode().mode
@@ -27,16 +33,18 @@ function Get_stl_mode()
   if m == 'n' or m == 'no' then
     fg = 'lightgreen'
   elseif m == 'i' or m == 'ic' then
-    fg = 'yellow'
+    fg = 'lightyellow'
   elseif m == 'v' or m == 'V' or m == '' then
-    fg = 'magenta'
+    fg = 'lightmagenta'
   elseif m == 'R' or m == 'Rv' then
-    fg = 'red'
+    fg = 'lightred'
   else
-    fg = 'cyan'
+    fg = 'lightcyan'
   end
-  vim.api.nvim_set_hl(0, 'StlMode', { fg = fg, bold = true })
-  return string.format('[ %s ]', modes[m] or '???'):upper()
+  vim.api.nvim_set_hl(0, 'StlMode', { fg = fg })
+  return string.format('[%s]', m or '???'):upper()
 end
 
-return '%#StlMode#%{v:lua.Get_stl_mode()}%#Normal# %q%f %y %h%r%m%w %=%S %l:%c %p%% %L Lines '
+vim.api.nvim_set_hl(0, 'StlText', {})
+
+return '%#StlMode#%{v:lua.Get_stl_mode()}%#StlText# %<%q%f %y %h%r%m%w %=%S %l:%c %p%% %L Lines '
