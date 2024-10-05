941b1a8aa3c9bea7f174e4778cf9617fdaffaa12
RadicalTray <hydrochloricmahn@gmail.com>
2024-10-05 21:58:58 +0700

    colors

--- colors/default-transparent.lua
+++ colors/default-transparent.lua
@@ -10,12 +10,12 @@ vim.g.terminal_color_6  = 'NvimLightCyan'
 vim.g.terminal_color_7  = 'NvimLightGrey2'
 
 vim.g.terminal_color_8  = 'NvimLightGrey4'
-vim.g.terminal_color_9  = '#d7005f'
-vim.g.terminal_color_10 = '#00af5f'
-vim.g.terminal_color_11 = '#fbb806'
-vim.g.terminal_color_12 = '#0087d7'
-vim.g.terminal_color_13 = '#f400a1'
-vim.g.terminal_color_14 = '#00afaf'
+vim.g.terminal_color_9  = '#feadcd'
+vim.g.terminal_color_10 = '#7ae7b9'
+vim.g.terminal_color_11 = '#ffec9e'
+vim.g.terminal_color_12 = '#5bd2f0'
+vim.g.terminal_color_13 = '#ffc6ff'
+vim.g.terminal_color_14 = '#9be7ff'
 vim.g.terminal_color_15 = 'NvimLightGrey1'
 
 vim.g.terminal_color_16 = 'NvimDarkGrey3'
@@ -28,6 +28,7 @@ vim.g.terminal_color_22 = 'NvimDarkCyan'
 vim.g.terminal_color_23 = 'NvimLightGrey3'
 
 vim.api.nvim_set_hl(0, 'Normal', { fg = 'NvimLightGrey2', bg = 'None' })
+vim.api.nvim_set_hl(0, 'Visual', { fg = 'fg',             bg = 'NvimDarkGrey4', ctermfg = 7, ctermbg = 0 })
 
 vim.api.nvim_set_hl(0, 'StlText',   { link = 'Normal' })
 vim.api.nvim_set_hl(0, 'StlMode',   { fg = 'NvimLightGreen', bold = true })
