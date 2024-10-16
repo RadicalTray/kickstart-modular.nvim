f6df889712aebcec6d2a2467f59daba285d85bb1
RadicalTray <hydrochloricmahn@gmail.com>
2024-10-16 19:52:42 +0700

    hi

--- lua/plugins/enabled/jupynium.lua
+++ lua/plugins/enabled/jupynium.lua
@@ -1,12 +1,10 @@
 return {
-  {
-    "kiyoon/jupynium.nvim",
-    -- enabled = false,
-    ft = 'python',
-    dependencies = {
-      "rcarriga/nvim-notify",
-      "stevearc/dressing.nvim",
-    },
-    build = { "source ~/.Python-venv/bin/activate; pip install ." }
-  },
+  "kiyoon/jupynium.nvim",
+  -- enabled = false,
+  ft = 'python',
+  -- dependencies = {
+  --   -- "rcarriga/nvim-notify", -- optional
+  --   -- "stevearc/dressing.nvim", -- optional, UI for :JupyniumKernelSelect
+  -- },
+  build = "source ~/.Python-venv/bin/activate; pip install .",
 }
