c674fdfd0cd2ec817bc27a97ae7ddec94fe01c57
RadicalTray <radicaltray@gmail.com>
2024-10-24 13:59:12 +0700

    update blink

--- lua/plugins/enabled/blink-cmp.lua
+++ lua/plugins/enabled/blink-cmp.lua
@@ -25,28 +25,13 @@ return {
       use_frecency = false,
     },
     highlight = {
-      -- sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- useful for when your theme doesn't support blink.cmp
-      -- will be removed in a future release, assuming themes add support
       use_nvim_cmp_as_default = true,
     },
-    -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-    -- adjusts spacing to ensure icons are aligned
-    nerd_font_variant = 'normal',
-
-    -- experimental auto-brackets support
-    -- accept = { auto_brackets = { enabled = true } }
-
-    -- experimental signature help support
-    -- trigger = { signature_help = { enabled = true } }
-
+    nerd_font_variant = 'mono',
     sources = {
-      providers = {
-        -- { 'blink.cmp.sources.lsp', name = 'LSP' },
-        { 'blink.cmp.sources.path', name = 'Path', score_offset = 3 },
-        -- { 'blink.cmp.sources.snippets', score_offset = -3 },
-        { 'blink.cmp.sources.buffer', name = 'Buffer' },
+      completion = { 
+        enabled_providers = { 'path', 'buffer' },
       },
     },
-  }
+  },
 }
