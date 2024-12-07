6d82ae09dae92a62712238a6f581ade012b4da60
RadicalTray <hydrochloricmahn@gmail.com>
2024-12-07 22:54:01 +0700

    cleanup config

--- lua/plugins/import/lsp.lua
+++ lua/plugins/import/lsp.lua
@@ -3,53 +3,6 @@ return {
     'neovim/nvim-lspconfig',
     cond = Plugin_enable_LSP,
     lazy = true,
-    config = function()
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-        callback = function(event)
-          -- vim.o.omnifunc = 'v:lua.MiniCompletion.completefunc_lsp' -- mini.completion
-
-          local map = function(lhs, rhs, desc)
-            vim.keymap.set('n', lhs, rhs, { buffer = event.buf, desc = 'LSP: ' .. desc })
-          end
-
-          -- local telescope_builtin = require 'telescope.builtin'
-          -- map('gd', telescope_builtin.lsp_definitions, '[G]oto [D]efinition')
-          -- map('gr', telescope_builtin.lsp_references, '[G]oto [R]eferences')
-          -- map('gI', telescope_builtin.lsp_implementations, '[G]oto [I]mplementation')
-          -- map('<leader>D', telescope_builtin.lsp_type_definitions, 'Type [D]efinition')
-          -- map('<leader>ds', telescope_builtin.lsp_document_symbols, '[D]ocument [S]ymbols')
-          -- map('<leader>ws', telescope_builtin.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-          -- map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-          -- map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-          local fzf = require 'fzf-lua'
-          map('gd', fzf.lsp_definitions, '[G]oto [D]efinition')
-          map('<leader>sr', fzf.lsp_references, '[S]earch [R]eferences')
-          map('gI', fzf.lsp_implementations, '[G]oto [I]mplementation')
-          map('<leader>ca', fzf.lsp_code_actions, '[C]ode [A]ction')
-          map('gD', fzf.lsp_declarations, '[G]oto [D]eclaration')
-
-          map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-          map('K', vim.lsp.buf.hover, 'Hover Documentation')
-
-          -- The following two autocommands are used to highlight references of the
-          -- word under your cursor when your cursor rests there for a little while.
-          -- local client = vim.lsp.get_client_by_id(event.data.client_id)
-          -- if client and client.server_capabilities.documentHighlightProvider then
-          --   vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-          --     buffer = event.buf,
-          --     callback = vim.lsp.buf.document_highlight,
-          --   })
-          --
-          --   vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-          --     buffer = event.buf,
-          --     callback = vim.lsp.buf.clear_references,
-          --   })
-          -- end
-        end,
-      })
-    end,
   },
   {
     'williamboman/mason-lspconfig.nvim',
@@ -59,21 +12,18 @@ return {
       'neovim/nvim-lspconfig',
       'williamboman/mason.nvim',
       'echasnovski/mini.nvim', -- for notify
-      { 'folke/neodev.nvim', opts = {} },
+      {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+          library = {
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
+      },
     },
     config = function()
       local server_configs = {
-        -- lua_ls = {
-        --   settings = {
-        --     Lua = {
-        --       -- mini.completion doesn't support snippet rn
-        --       -- completion = {
-        --       --   callSnippet = 'Replace',
-        --       -- },
-        --       -- diagnostics = { disable = { 'missing-fields' } },
-        --     },
-        --   },
-        -- },
         pylsp = {
           settings = {
             pylsp = {
@@ -109,7 +59,6 @@ return {
 
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities.textDocument.completion.completionItem.snippetSupport = false
-      -- capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
       require('mason-lspconfig').setup {
         handlers = {
--- lua/plugins/import/mini.lua
+++ lua/plugins/import/mini.lua
@@ -4,6 +4,7 @@ return {
   version = false,
   config = function()
     require('mini.icons').setup {}
+
     require('mini.notify').setup {
       window = {
         -- Floating window config
@@ -19,51 +20,8 @@ return {
       },
     }
     vim.notify = require('mini.notify').make_notify()
-    -- require('mini.animate').setup {}
-    -- require('mini.ai').setup {}
-    -- require('mini.surround').setup {}
-
-    -- require('mini.completion').setup {
-    --   -- delay = { completion = 0, info = 0, signature = 0 },
-    --   lsp_completion = {
-    --     source_func = 'omnifunc',
-    --     auto_setup = false,
-    --     ---@diagnostic disable-next-line: unused-local
-    --     -- process_items = function(items, base)
-    --     --   return {}
-    --     -- end,
-    --   },
-    --   fallback_action = '<c-x><c-n>',
-    -- }
-    -- vim.keymap.set('i', '<cr>', function()
-    --   return vim.fn.pumvisible() ~= 0 and '<c-y><cr>' or '<cr>'
-    -- end, { expr = true })
 
     require('mini.git').setup {}
-    vim.keymap.set('n', '<leader>gl', '<cmd>Git log<CR>', {
-      desc = 'Open [G]it [L]og',
-    })
-    vim.keymap.set('n', '<leader>gla', '<cmd>Git log --all<CR>', {
-      desc = 'Open [G]it [L]og [A]ll',
-    })
-    vim.keymap.set('n', '<leader>glg', '<cmd>Git log --graph<CR>', {
-      desc = 'Open [G]it [L]og [G]raph',
-    })
-    vim.keymap.set('n', '<leader>glga', '<cmd>Git log --graph --all<CR>', {
-      desc = 'Open [G]it [L]og [G]raph [A]ll',
-    })
-    vim.keymap.set('n', '<leader>glo', '<cmd>Git log --oneline<CR>', {
-      desc = 'Open [G]it [L]og [O]neline',
-    })
-    vim.keymap.set('n', '<leader>gloa', '<cmd>Git log --oneline --all<CR>', {
-      desc = 'Open [G]it [L]og [O]neline [A]ll',
-    })
-    vim.keymap.set('n', '<leader>glog', '<cmd>Git log --oneline --graph<CR>', {
-      desc = 'Open [G]it [L]og [O]neline [G]raph',
-    })
-    vim.keymap.set('n', '<leader>gloga', '<cmd>Git log --oneline --graph --all<CR>', {
-      desc = 'Open [G]it [L]og [O]neline [G]raph [A]ll',
-    })
 
     require('mini.diff').setup {
       view = {
@@ -74,35 +32,6 @@ return {
     vim.keymap.set('n', '<leader>gh', MiniDiff.toggle_overlay, {
       desc = 'Toggle [G]it [H]unk overlay',
     })
-
-    -- require('mini.visits').setup {}
-    -- BUG: Zellij(?) stops <c-j> from working (becomes ^M), and doesn't support CTRL+SHIFT (probably zellij)
-    -- vim.keymap.set('n', '<c-j>', function()
-    --   MiniVisits.iterate_paths('backward', nil, { wrap = true })
-    -- end, {
-    --   desc = 'Go to previous path',
-    -- })
-    -- vim.keymap.set('n', '<c-k>', function()
-    --   MiniVisits.iterate_paths('forward', nil, { wrap = true })
-    -- end, {
-    --   desc = 'Go to next path',
-    -- })
-    -- vim.keymap.set('n', '<leader>sp', MiniVisits.select_path, {
-    --   desc = '[S]earch visited [P]aths',
-    -- })
-    -- vim.keymap.set('n', '<leader>rp', MiniVisits.remove_path, {
-    --   desc = '[R]emove [P]ath',
-    -- })
-    --
-    -- vim.keymap.set('n', '<leader>al', MiniVisits.add_label, {
-    --   desc = '[A]dd [L]abel',
-    -- })
-    -- vim.keymap.set('n', '<leader>rl', MiniVisits.remove_label, {
-    --   desc = '[R]emove [L]abel',
-    -- })
-    -- vim.keymap.set('n', '<leader>sl', MiniVisits.select_label, {
-    --   desc = '[S]earch [L]abels',
-    -- })
   end,
 }
 -- vim: ts=2 sts=2 sw=2 et
