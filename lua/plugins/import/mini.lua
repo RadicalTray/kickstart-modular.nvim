cc4e90bb96eeaa1e98428458f67164f9378246b6
RadicalTray <radicaltray@gmail.com>
2025-01-01 13:29:32 +0700

    disable mini hipatterns

--- lua/plugins/import/mini.lua
+++ lua/plugins/import/mini.lua
@@ -4,40 +4,40 @@ return {
   config = function()
     require('mini.icons').setup {}
 
-    local function fn(words)
-      local tbl = {}
-      for _, word in ipairs(words) do
-        table.insert(tbl, string.format('%s%s%s', '%f[%w]()', word, '()%f[%W]'))
-      end
-      return tbl
-    end
-
-    local hipatterns = require('mini.hipatterns')
-    hipatterns.setup {
-      highlighters = {
-        error = {
-          pattern = fn { 'FIX', 'FIXME', 'BUG', },
-          group = 'Error'
-        },
-        warn  = {
-          pattern = fn { 'WARN', },
-          group = 'Warn'
-        },
-        todo  = {
-          pattern = fn { 'TODO', },
-          group = 'Todo'
-        },
-        hack  = {
-          pattern = fn { 'HACK', 'PERF', },
-          group = 'Hack'
-        },
-        note  = {
-          pattern = fn { 'NOTE', },
-          group = 'Note'
-        },
-        hex_color = hipatterns.gen_highlighter.hex_color(),
-      },
-    }
+    -- local function fn(words)
+    --   local tbl = {}
+    --   for _, word in ipairs(words) do
+    --     table.insert(tbl, string.format('%s%s%s', '%f[%w]()', word, '()%f[%W]'))
+    --   end
+    --   return tbl
+    -- end
+    --
+    -- local hipatterns = require('mini.hipatterns')
+    -- hipatterns.setup {
+    --   highlighters = {
+    --     error = {
+    --       pattern = fn { 'FIX', 'FIXME', 'BUG', },
+    --       group = 'Error'
+    --     },
+    --     warn  = {
+    --       pattern = fn { 'WARN', },
+    --       group = 'Warn'
+    --     },
+    --     todo  = {
+    --       pattern = fn { 'TODO', },
+    --       group = 'Todo'
+    --     },
+    --     hack  = {
+    --       pattern = fn { 'HACK', 'PERF', },
+    --       group = 'Hack'
+    --     },
+    --     note  = {
+    --       pattern = fn { 'NOTE', },
+    --       group = 'Note'
+    --     },
+    --     hex_color = hipatterns.gen_highlighter.hex_color(),
+    --   },
+    -- }
 
     require('mini.notify').setup {
       window = {
